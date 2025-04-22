4b4be9f1fc8db886028f0ccec6451b17942ef4b4
Alex-V0l <lastvolnov@gmail.com>
2025-04-22 16:13:49 +0300

    Properties_added_correctly

--- src/test/java/patterns/builder/Hero.java
+++ src/test/java/patterns/builder/Hero.java
@@ -1,25 +1,25 @@
-//package patterns.builder;
-//
-//import lombok.Builder;
-//import lombok.Data;
-//
-//import java.util.List;
-//
-//@Data
-//@Builder(toBuilder = true)
-//public class Hero {
-//    private String name;
-//    private String className;
-//    private String race;
-//    private int strength;
-//    private int dexterity;
-//    private int constitution;
-//    private int wisdom;
-//    private int intelligence;
-//    private int charisma;
-//    private boolean isEvil;
-//    private double expModifier;
-//    private List<String> startEquip;
-//    private char sex;
-//}
-//
+package patterns.builder;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder(toBuilder = true)
+public class Hero {
+    private String name;
+    private String className;
+    private String race;
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int wisdom;
+    private int intelligence;
+    private int charisma;
+    private boolean isEvil;
+    private double expModifier;
+    private List<String> startEquip;
+    private char sex;
+}
+
